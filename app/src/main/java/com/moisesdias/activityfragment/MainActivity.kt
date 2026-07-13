4c1ceccd04172476882a2c80acdb9fb1137f6512
Moises Dias <m.dias009@gmail.com>
2026-07-13 12:03:09 -0300

    Initialize Android project with basic activity navigation
    - Create `MainActivity` with logic to launch a details screen via `Intent`.
    - Create `DetalhesActivity` with functionality to close the screen.
    - Implement UI layouts for both activities using `ConstraintLayout`.
    - Configure project build using Gradle Kotlin DSL and Version Catalogs.
    - Add standard Android boilerplate, including manifest declarations, resource files, and adaptive icons.

---
+++ app/src/main/java/com/moisesdias/activityfragment/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.moisesdias.activityfragment
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var btnAbrir: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+
+        btnAbrir = findViewById(R.id.btn_abrir)
+        btnAbrir.setOnClickListener {
+
+            val intent = Intent(
+                this,
+                DetalhesActivity::class.java
+            )
+
+            //Iniciar uma nova tela/Activity
+            startActivity(intent)
+        }
+    }
+}
